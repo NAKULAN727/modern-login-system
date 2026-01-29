6846de4f9d1689abde3d7c67350180b6d47a13b9
Nakulan <nakulan07022007@gmail.com>
2026-01-29 09:51:01 +0530

    Added the codes for login.html/register.html and login.js/register.js

---
+++ js/register.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    $("#registerBtn").click(function () {
+
+        let email = $("#email").val();
+        let password = $("#password").val();
+        let confirmPassword = $("#confirm_password").val();
+
+        // 1️⃣ Validation
+        if (email === "" || password === "" || confirmPassword === "") {
+            alert("All fields are required");
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+
+        $.ajax({
+            url:"php/register.php",
+            type:"POST",
+            data:{
+                email:email,
+                password:password,
+            },
+            dataType:"json",
+            success:function(res){
+                if(res.status==200)
+                {
+                    alert("Registration Success");
+                    window.location.href="login.html";
+                }
+                else{
+                    alert("Registration Failed");
+                }
+            },
+            error:function(){
+                alert("Server Error");
+            }
+        });     
+    });
+});
